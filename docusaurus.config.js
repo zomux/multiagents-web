f46f653cefe346596b45bd59659cc9802556686e
Raphael Shu <shu@deeplearn.org>
2024-09-23 01:16:41 -0700

    Updating

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,8 +112,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Workshop',
+                to: '/docs/workshop',
               },
             ],
           },
